a6a5e8372b90eb754e4af52dc7f6680261c03bc0
akshat-shah05 <akshatshah1605@gmail.com>
2024-02-11 20:32:54 -0500

    landing and dashboard

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
     return (
-        <div> landing page (unprotected) </div>
+        <div> 
+            landing page (unprotected) 
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Login
+                    </Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button>
+                        Register
+                    </Button>
+                </Link>
+            </div>
+        </div>
     );
 }
 
